8e5423def4fdd01b12b952cdd45a072424f63c3b
satish <satishkonduru0225@gmail.com>
2024-08-02 21:19:22 +0530

    header completed

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+
+
+  login(){
+
+  }
+  register(){
+    
+  }
 }
