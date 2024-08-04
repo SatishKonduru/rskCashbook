674bc704d6fe1e09a90a6f5c269b4f234e4a5844
satish <satishkonduru0225@gmail.com>
2024-08-04 08:31:04 +0530

    logged username display is done

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,24 +1,44 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, inject, OnInit, signal } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import { Router, RouterModule } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatButtonModule, RouterModule],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, RouterModule, MatMenuModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
   router = inject(Router)
-
-
+  private _user = inject(UserService)
+  public username: any = signal('')
+  ngOnInit(): void {
+    this.getUser()
+  }
   login(){
     this.router.navigate(['/login'])
   }
   register(){
     this.router.navigate(['/register'])
+  }
+  ngAfterViewChecked(){
+    this.getUser()
+  }
+  getUser(){
+    const user = sessionStorage.getItem('userCredentials') 
+    if(user){
+      const userDetails = this._user.retrieveCredentials().username
+      this.username.update(username => username = userDetails)
+      console.log("Logged username: ", userDetails)
+    }
+  }
+  logout(){
+    sessionStorage.removeItem('userCredentials')
+    this.username.update(u => u = '')
   }
 }
