47a813b55b936d8a26cafdff3423de406210b198
satish <satishkonduru0225@gmail.com>
2024-08-06 17:53:47 +0530

    Working on view Book

--- src/app/pages/view-book/view-book.component.ts
+++ src/app/pages/view-book/view-book.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -7,16 +7,20 @@ import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
+import {MatDrawer, MatSidenavModule} from '@angular/material/sidenav';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-view-book',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule, MatTooltipModule, MatFormFieldModule,MatInputModule, FormsModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatTooltipModule, MatFormFieldModule,MatInputModule, FormsModule, MatSidenavModule, MatDividerModule, MatToolbarModule],
   templateUrl: './view-book.component.html',
   styleUrl: './view-book.component.css',
   preserveWhitespaces: true
 })
 export class ViewBookComponent {
+  isDrawerOpen = false;
   activatedRoute = inject(ActivatedRoute)
   bookName: any
   router = inject(Router)
@@ -27,5 +31,8 @@ export class ViewBookComponent {
 
   goBack(){
     this.router.navigate(['/dashboard'])
+  }
+  toggleDrawer(): void {
+    this.isDrawerOpen = !this.isDrawerOpen;
   }
 }
