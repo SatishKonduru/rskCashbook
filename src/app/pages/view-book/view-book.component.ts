03af185c98b7029df5298838f15b69cb5bb70b8d
satish <satishkonduru0225@gmail.com>
2024-08-06 18:32:38 +0530

    Working on Add Form

--- src/app/pages/view-book/view-book.component.ts
+++ src/app/pages/view-book/view-book.component.ts
@@ -1,34 +1,46 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, ViewChild } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import { ActivatedRoute, Router } from '@angular/router';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
-import {MatDrawer, MatSidenavModule} from '@angular/material/sidenav';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCalendar, MatDatepickerModule} from '@angular/material/datepicker';
+import {NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
 
 @Component({
   selector: 'app-view-book',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule, MatTooltipModule, MatFormFieldModule,MatInputModule, FormsModule, MatSidenavModule, MatDividerModule, MatToolbarModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatTooltipModule, MatFormFieldModule,MatInputModule, FormsModule, MatSidenavModule, MatDividerModule, MatToolbarModule, ReactiveFormsModule, MatDatepickerModule, MatCalendar, NgxMaterialTimepickerModule],
   templateUrl: './view-book.component.html',
   styleUrl: './view-book.component.css',
   preserveWhitespaces: true
 })
-export class ViewBookComponent {
+export class ViewBookComponent implements OnInit{
   isDrawerOpen = false;
   activatedRoute = inject(ActivatedRoute)
   bookName: any
   router = inject(Router)
   searchKey: any = ''
+  addFrom : any = FormGroup
+  fb = inject(FormBuilder)
+
   constructor(){
     this.activatedRoute.queryParams.subscribe(p => this.bookName = p['book'])
   }
-
+  ngOnInit(): void {
+    this.addFrom = this.fb.group({
+      date: [new Date(), Validators.required],
+      time: ['', Validators.required],
+      amount: [0, Validators.required],
+      description: ['', Validators.required]
+    })
+  }
   goBack(){
     this.router.navigate(['/dashboard'])
   }
